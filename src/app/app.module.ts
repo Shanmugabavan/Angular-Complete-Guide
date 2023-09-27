e5ce2fff62cce1df64edf59c7465988abf235220
Shanmugabavan <shanmugabavan.developer@gmail.com>
2023-09-27 20:41:20 +0530

    Upto 02_34 two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from "./Server/server-component";
+import { ServersComponent } from './Servers/servers.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
